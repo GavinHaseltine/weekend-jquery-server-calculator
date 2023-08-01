3d88650736ea9d6f79368196418aff6396408635
Gavin Haseltine <gavinhaseltine@gmail.com>
2023-07-31 20:52:03 -0500

    addedGET to client

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,74 +1,111 @@
+const { response } = require("express");
+
 $(document).ready(handleReady);
 
 //Global Vars
-let symbolType;
-let outcome;
+// let symbolType;
+// let outcome;
+let symbol;
 
 function handleReady() {
   console.log("jquery is loaded!")
-   $('#equalsButton').on('click',handleEquals);
-   $('#clearButton').on('click',handleClear);
-   $('#plusButton').on('click', makePlus);
-   $('#subButton').on('click', makeMinus);
-   $('#multiplyButton').on('click', makeMultiply);
-   $('#divideButton').on('click', makeDivide);
+
+  getCalcHistory();
+//    $('#equalsButton').on('click',handleEquals);
+//    $('#clearButton').on('click',handleClear);
+//    $('#plusButton').on('click', makePlus);
+//    $('#subButton').on('click', makeMinus);
+//    $('#multiplyButton').on('click', makeMultiply);
+//    $('#divideButton').on('click', makeDivide);
+}
+
+
+function getCalcHistory(){
+
+    $.ajax({
+        method: 'GET',
+        url: '/calc'
+    }).then((response) => {
+        console.log('In GET', response);
+        render(response);
+
+        let currentResult = response[response.length - 1]?.result
+
+        if (currentResult == undefined) {
+            currentResult = ''
+        }
+
+        renderCurrentResult(currentResult)
+    }).catch((error) => {
+        alert("Error with GET /calc");
+    })
+}
+
+function renderCurrentResult(currentResult) {
+    $('#historyArea').empty()
+    $('#historyArea').append(`${currentResult}`)
 }
 
 
 
 
-function handleEquals(){
+function render(bigMath){
+    console.log('in render')
 
-    $('#outputArea').text(" ");
+}
 
-    let firstNumber = Number($('#inputOne').val());
+// function handleEquals(){
+
+//     $('#outputArea').text(" ");
+
+//     let firstNumber = Number($('#inputOne').val());
     
-    let secondNumber = Number($('#inputTwo').val());
+//     let secondNumber = Number($('#inputTwo').val());
     
-    if(symbolType == "plus"){
-    outcome = firstNumber + secondNumber;
-    //console.log(outcome);
-    $('#outputArea').append(outcome);
-    $('#historyArea').append(`<ul>${firstNumber} + ${secondNumber} = ${outcome}</ul>`);
-    } else if(symbolType == "minus"){
-        outcome = firstNumber - secondNumber;
-    //console.log(outcome);
-    $('#outputArea').append(outcome);
-    $('#historyArea').append(`<ul>${firstNumber} - ${secondNumber} = ${outcome}</ul>`);
-    } else if(symbolType == "multiply"){
-        outcome = firstNumber * secondNumber;
-        //console.log(outcome);
-        $('#outputArea').append(outcome);
-        $('#historyArea').append(`<ul>${firstNumber} * ${secondNumber} = ${outcome}</ul>`);
-    } else if(symbolType == "divide"){
-        outcome = firstNumber / secondNumber;
-        //console.log(outcome);
-        $('#outputArea').append(outcome);
-        $('#historyArea').append(`<ul>${firstNumber} / ${secondNumber} = ${outcome}</ul>`);
-    }
+//     if(symbolType == "plus"){
+//     outcome = firstNumber + secondNumber;
+//     //console.log(outcome);
+//     $('#outputArea').append(outcome);
+//     $('#historyArea').append(`<ul>${firstNumber} + ${secondNumber} = ${outcome}</ul>`);
+//     } else if(symbolType == "minus"){
+//         outcome = firstNumber - secondNumber;
+//     //console.log(outcome);
+//     $('#outputArea').append(outcome);
+//     $('#historyArea').append(`<ul>${firstNumber} - ${secondNumber} = ${outcome}</ul>`);
+//     } else if(symbolType == "multiply"){
+//         outcome = firstNumber * secondNumber;
+//         //console.log(outcome);
+//         $('#outputArea').append(outcome);
+//         $('#historyArea').append(`<ul>${firstNumber} * ${secondNumber} = ${outcome}</ul>`);
+//     } else if(symbolType == "divide"){
+//         outcome = firstNumber / secondNumber;
+//         //console.log(outcome);
+//         $('#outputArea').append(outcome);
+//         $('#historyArea').append(`<ul>${firstNumber} / ${secondNumber} = ${outcome}</ul>`);
+//     }
 
 
-}
+// }
 
-function handleClear(){
-    $('#inputOne').val('')
-    $('#inputTwo').val('')
-    $('#outputArea').text('')
-console.log('In handleClear');
-}
+// function handleClear(){
+//     $('#inputOne').val('')
+//     $('#inputTwo').val('')
+//     $('#outputArea').text('')
+// console.log('In handleClear');
+// }
 
-function makePlus(){
-symbolType = "plus";
-}
+// function makePlus(){
+// symbolType = "plus";
+// }
 
-function makeMinus(){
-    symbolType = "minus";
-}
+// function makeMinus(){
+//     symbolType = "minus";
+// }
 
-function makeMultiply(){
-    symbolType = "multiply";
-}
+// function makeMultiply(){
+//     symbolType = "multiply";
+// }
 
-function makeDivide(){
-    symbolType = "divide";
-}
+// function makeDivide(){
+//     symbolType = "divide";
+// }
--- server/server.js
+++ server/server.js
@@ -7,6 +7,16 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 
 app.use(express.static('server/public'));
+app.use(bodyParser.json());
+
+let history = []
+app.get('/calc', (req, res) => {
+    console.log("in server, /calc", history)
+    res.send(history)
+})
+
+
+
 
 
 
